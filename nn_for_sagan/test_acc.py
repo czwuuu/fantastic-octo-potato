c3bdbc575f19dd575f273992fea6d1a093d68ace
Lyw158 <1702311094@qq.com>
2025-02-24 20:10:25 +0800

    1

--- nn_for_sagan/test_acc.py
+++ nn_for_sagan/test_acc.py
@@ -15,3 +15,11 @@ def calculate_accuracy(model, dataloader):
             total_samples += inputs.size(0)
     accuracy = total_correct / total_samples
     return accuracy
+
+if __name__ == '__main__':
+    outputs = torch.tensor([[1, 2, 3, 4]])
+    targets = torch.tensor([[1.04, 2.03, 3.03, 4.03]])
+    relative_error = torch.abs(outputs - targets) / torch.abs(targets)
+    # 判断所有参数的相对误差是否小于 3%
+    correct = torch.all(relative_error < 0.03, dim=1).sum().item()
+    print(correct)
